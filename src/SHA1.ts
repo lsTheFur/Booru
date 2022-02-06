2e832c73488f26459e8e817dfeccfe0854d312f9
lsTheFur <ls@wafflepowered.com>
2022-02-06 08:26:34 +0100

    [Refactor] Minor Refactor for SHA1.ts

--- src/SHA1.ts
+++ src/SHA1.ts
@@ -1,3 +1,4 @@
 import * as crypto from 'crypto';
 const hash = crypto.createHash('sha1');
-export default (text: string) => hash.update(text).digest('hex');
+export const Sha1 = (text: string) => hash.update(text).digest('hex');
+export default Sha1;
